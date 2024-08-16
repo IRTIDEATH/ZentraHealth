f8eb8e1c46dbc19fba5e8e6e505740d55de7897c
IRTIDEATH <enderjhass@gmail.com>
2024-08-16 09:58:42 +0700

    upd: create navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { FloatingNav } from './components/ui/floating-navbar.jsx'
+import { navItems } from './constants/index.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <FloatingNav navItems={navItems}/>
     <App />
   </StrictMode>,
 )
