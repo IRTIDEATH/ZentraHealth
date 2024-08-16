8c8fb218011981d96b0d76aa1bf44b72d89bd7ac
IRTIDEATH <enderjhass@gmail.com>
2024-08-16 11:18:45 +0700

    upd: accordion changed

--- src/components/Nub1.jsx
+++ src/components/Nub1.jsx
@@ -10,22 +10,45 @@ import {
 const Nub1 = () => {
   return (
     <div className="w-full h-screen bg-[#FD5852] rounded-tr-[6rem]">
-        <div className="static container flex items-center justify-end pt-2">
-            <div className="absolute flex pr-12">
-            <img src={plus} alt="" />
-            </div>
+      <div className="static container flex items-center justify-end pt-2">
+        <div className="absolute flex pr-12">
+          <img src={plus} alt="" />
         </div>
-        <div className="flex flex-col p-10">
-            <div className="flex flex-col pt-6 space-y-5">
-                <h1 className="text-5xl text-white font-bold">Sehat Fisik</h1>
-                <p className="text-white text-xl font-normal text-balance w-[600px]">
-                Sebagai upaya pengurangan resiko obesitas serta meningkatkan
-                kualitas kesehatan fisik peserta didik, maka perlu untuk
-                dilaksanakannya aktivitas fisik secara rutin. Apa saja aktivitas
-                fisik yang dapat kita upayakan di lingkungan sekolah?
-                </p>
-            </div>
-            </div>
+      </div>
+      <div className="flex flex-col p-10">
+        <div className="flex flex-col pt-6 space-y-5 mb-20">
+          <h1 className="text-5xl text-white font-bold">Sehat Fisik</h1>
+          <p className="text-white text-xl font-normal text-balance w-[600px]">
+            Sebagai upaya pengurangan resiko obesitas serta meningkatkan
+            kualitas kesehatan fisik peserta didik, maka perlu untuk
+            dilaksanakannya aktivitas fisik secara rutin. Apa saja aktivitas
+            fisik yang dapat kita upayakan di lingkungan sekolah?
+          </p>
+        </div>
+
+        <div className="px-20 py-10 border-2 border-white rounded-2xl">
+          <Accordion type="single" collapsible className="w-full text-[#ECFFFD]">
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="text-2xl">Is it accessible?</AccordionTrigger>
+              <AccordionContent className="text-xl">
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger className="text-2xl">Is it accessible?</AccordionTrigger>
+              <AccordionContent className="text-xl">
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger className="text-2xl">Is it accessible?</AccordionTrigger>
+              <AccordionContent className="text-xl">
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
     </div>
   );
 };
