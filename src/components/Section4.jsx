4a9613950898118029b7f57c90e31b2905866333
Feblcsack <alifianabdiel@gmail.com>
2024-08-20 19:44:26 +0700

    upd: adding 3d logo

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -11,9 +11,21 @@ import { useScroll, useTransform } from "framer-motion";
 import { motion } from "framer-motion";
 
 const Section4 = () => {
-  const {scrollYProgress} = useScroll()
+  const { scrollYProgress } = useScroll();
 
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, 360])
+  const bounce = {
+    initial: { y: 0 },
+    animate: {
+      y: [0, -20, 0],
+      transition: {
+        duration: 0.6,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatType: "loop",
+      },
+    },
+  };
+
   return (
     <div className="bg-[#FD5852]" id="bergizi">
       <div className="w-full bg-[#ECFFFD] md:rounded-tl-[5rem] rounded-tl-[4rem] pb-16 md:pb-28">
@@ -23,7 +35,9 @@ const Section4 = () => {
               src={pluss}
               alt=""
               className="w-[90px] h-[90px] md:w-[140px] md:h-[140px] object-contain shadowplus"
-              style={{rotate}}
+              variants={bounce}
+              initial="initial"
+              animate="animate"
             />
           </div>
         </div>
@@ -34,12 +48,12 @@ const Section4 = () => {
                 Sehat Bergizi
               </h1>
               <p className="text-[#1E1E1E] md:text-xl text-base text-start font-normal text-wrap md:w-[480px]">
-              Meningkatkan kesehatan peserta didik melalui edukasi pola makan yang tepat dan bergizi.
-              Guna meningkatkan derajat kesehatan peserta didik, maka perlu diterapkannya pola makan yang tepat dan konsumsi makanan bergizi. Bagaimana langkah yang perlu kita gerakkan untuk mencapai kondisi kesehatan peserta didik yang ideal?
+                Meningkatkan kesehatan peserta didik melalui edukasi pola makan yang tepat dan bergizi.
+                Guna meningkatkan derajat kesehatan peserta didik, maka perlu diterapkannya pola makan yang tepat dan konsumsi makanan bergizi. Bagaimana langkah yang perlu kita gerakkan untuk mencapai kondisi kesehatan peserta didik yang ideal?
               </p>
             </div>
             <div className="w-[400px] mt-6">
-              <BuahCanvas/>
+              <BuahCanvas />
             </div>
           </div>
 
@@ -54,13 +68,13 @@ const Section4 = () => {
                 className="border-b border-[#1E1E1E]"
               >
                 <AccordionTrigger className="md:text-xl text-base">
-                Edukasi Kegiatan Sehat Bergizi
+                  Edukasi Kegiatan Sehat Bergizi
                 </AccordionTrigger>
                 <AccordionContent className="md:text-lg text-sm">
-                ● Pembiasaan makan dan minum dengan gizi seimbang termasuk minum air putih, makan buah dan sayur setiap hari.<br></br>
-                ● Peningkatan pemahaman Gizi Seimbang (Isi Piringku).<br></br>
-                ● Meminimalisir konsumsi makanan cepat saji, berpengawet, tinggi gula dan kolesterol.<br></br>
-                ● Membina kantin yang sehat.
+                  ● Pembiasaan makan dan minum dengan gizi seimbang termasuk minum air putih, makan buah dan sayur setiap hari.<br />
+                  ● Peningkatan pemahaman Gizi Seimbang (Isi Piringku).<br />
+                  ● Meminimalisir konsumsi makanan cepat saji, berpengawet, tinggi gula dan kolesterol.<br />
+                  ● Membina kantin yang sehat.
                 </AccordionContent>
               </AccordionItem>
               <AccordionItem
@@ -68,11 +82,18 @@ const Section4 = () => {
                 className="border-b border-[#1E1E1E]"
               >
                 <AccordionTrigger className="md:text-xl text-base">
-                Isi Piringku.
+                  Isi Piringku.
                 </AccordionTrigger>
                 <AccordionContent className="md:text-lg text-sm">
-                  <img src="/public/assets/content/isipiring1.svg" alt="" srcset="" />
-                  <a href="https://ditpsd.kemdikbud.go.id/kss/wp-content/uploads/2023/05/isi-piringku-1-1024x1024.png" class="text-gray-500 italic">https://ditpsd.kemdikbud.go.id/kss/wp-content/uploads/2023/05/isi-piringku-1-1024x1024.png</a><br/><br/>
+                  <img src="/public/assets/content/isipiring1.svg" alt="" />
+                  <a
+                    href="https://ditpsd.kemdikbud.go.id/kss/wp-content/uploads/2023/05/isi-piringku-1-1024x1024.png"
+                    className="text-gray-500 italic"
+                  >
+                    https://ditpsd.kemdikbud.go.id/kss/wp-content/uploads/2023/05/isi-piringku-1-1024x1024.png
+                  </a>
+                  <br />
+                  <br />
                   Dalam satu piring setiap kali makan, setengah piring diisi dengan sayur dan buah, sedangkan setengah lainnya diisi dengan makanan pokok dan lauk pauk. Tak lupa juga untuk mengonsumsi 8 gelas air setiap hari, melakukan aktivitas fisik 30 menit setiap hari, dan mencuci tangan dengan air dan sabun sebelum dan setelah makan.
                 </AccordionContent>
               </AccordionItem>
@@ -81,17 +102,25 @@ const Section4 = () => {
                 className="border-b border-[#1E1E1E]"
               >
                 <AccordionTrigger className="md:text-xl text-base">
-                Komponen Kantin Sehat
+                  Komponen Kantin Sehat
                 </AccordionTrigger>
                 <AccordionContent className="md:text-lg text-sm">
-                <img src="/public/assets/content/isipiring2.svg" alt="" srcset="" />
-                <a href="https://ditpsd.kemdikbud.go.id/kss/wp-content/uploads/2023/05/4-pilar-kantin-sehat-sekolah-1024x1024.png" class="text-gray-500 italic">https://ditpsd.kemdikbud.go.id/kss/wp-content/uploads/2023/05/4-pilar-kantin-sehat-sekolah-1024x1024.png
-                </a><br/><br/>
-                    ● Komitmen dan Manajemen Sekolah : Memonitor konsumsi jajanan yang disediakan di kantin sekolah.
-                <br/>● Sumber Daya Manusia : Para penjual memiliki pengetahuan serta keterampilan mengelola kantin dan jajanan sehat di sekolah.
-                <br/>● Sarana dan Prasarana : Menjamin kebersihan peralatan makan di kantin sekolah serta lingkungan kantin.
-                <br/>● Mutu Pangan : Jajanan yang dijual wajib memenuhi standar aman, sehat, serta bergizi.
-
+                  <img src="/public/assets/content/isipiring2.svg" alt="" />
+                  <a
+                    href="https://ditpsd.kemdikbud.go.id/kss/wp-content/uploads/2023/05/4-pilar-kantin-sehat-sekolah-1024x1024.png"
+                    className="text-gray-500 italic"
+                  >
+                    https://ditpsd.kemdikbud.go.id/kss/wp-content/uploads/2023/05/4-pilar-kantin-sehat-sekolah-1024x1024.png
+                  </a>
+                  <br />
+                  <br />
+                  ● Komitmen dan Manajemen Sekolah : Memonitor konsumsi jajanan yang disediakan di kantin sekolah.
+                  <br />
+                  ● Sumber Daya Manusia : Para penjual memiliki pengetahuan serta keterampilan mengelola kantin dan jajanan sehat di sekolah.
+                  <br />
+                  ● Sarana dan Prasarana : Menjamin kebersihan peralatan makan di kantin sekolah serta lingkungan kantin.
+                  <br />
+                  ● Mutu Pangan : Jajanan yang dijual wajib memenuhi standar aman, sehat, serta bergizi.
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { ReactTyped } from "react-typed";
+
+// Component untuk memuat dan menampilkan model 3D GLTF
+function Model({ url }) {
+  const { scene } = useGLTF(url);
+  return <primitive object={scene} scale={2.8} />;
+}
 
 const Welcome = () => {
   return (
-    <div className="w-full">
-      <div className="flex flex-col md:px-16 px-7 pt-20 pb-[17rem] md:pb-52 leading-none" id="tentang">
+    <div className="w-full flex flex-col md:flex-row">
+      <div className="flex flex-col md:px-16 px-7 pt-20 pb-[10rem] md:pb-52 leading-none md:w-1/2" id="tentang">
         <h1 className="text-2xl md:text-4xl font-bold">
           Bersama <span className="text-[#FD5852]">ZentraHealth</span> <br />
           Wujudkan{" "}
@@ -23,17 +31,29 @@ const Welcome = () => {
             />
           </span>
         </h1>
-        <div className="pl-5 py-2 border-l-4 border-black mt-4 text-[#1E1E1E] space-y-4">
+        <div className="pl-5 py-2 border-l-4 border-black mt-3 text-[#1E1E1E] space-y-4">
           <p className="text-wrap leading-5 max-w-[600px] font-normal text-base">
-            Kondisi kesehatan pelajar saat ini cukup mengkhawatirkan. Pihak
-            Usaha Kesehatan Sekolah membuka program Gerakan Sekolah Sehat (GSS).
-            ZentraHealth, turut berkontribusi aktif dalam mewujudkan program
-            Gerakan Sekolah Sehat dengan menyajikan 5 fokus utama mulai dari
-            Sehat Fisik, Sehat Bergizi, Sehat Imunisasi, Sehat Mental, hingga
-            Sehat Lingkungan.
+            Kondisi kesehatan pelajar saat ini cukup mengkhawatirkan. Pihak Usaha Kesehatan Sekolah membuka program Gerakan Sekolah Sehat (GSS). ZentraHealth, turut berkontribusi aktif dalam mewujudkan program Gerakan Sekolah Sehat dengan menyajikan 5 fokus utama mulai dari Sehat Fisik, Sehat Bergizi, Sehat Imunisasi, Sehat Mental, hingga Sehat Lingkungan.
           </p>
           <p className="text-wrap font-medium text-lg">“Ayo! Bergerak bersama mewujudkan generasi yang sehat, kuat, cerdas, dan berkarakter!”</p>
         </div>
+      </div>
+
+      {/* Bagian sebelah kanan untuk asset 3D */}
+      <div className="md:w-1/2 w-full h-[500px] md:h-[500px] mt-6 md:mt-0 flex items-center justify-center">
+        <Canvas className="!px-1 md:!px-0"> {/* Mengurangi padding pada perangkat mobile */}
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[0, 5, 5]} />
+          <OrbitControls
+            autoRotate={true}
+            autoRotateSpeed={5}
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+            rotateSpeed={0.4}
+          />
+          <Model url="/3d/logo.glb" />
+        </Canvas>
       </div>
     </div>
   );
